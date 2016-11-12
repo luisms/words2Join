6c0f48997fa92659d6c29e986f03bbf007662eaa
Micro-N-PC\Nacho <yoiiii121@gmail.com>
2016-11-12 19:50:13 +0100

    Arreglar errores

--- Clasificacion.js
+++ Clasificacion.js
@@ -1,44 +1,58 @@
+var express = require("express");
+var bodyparser = require("body-parser");
 var app = express();
 var apiBaseURL = "/api/v1";
 
 var jugador1 = { ranking: 1, puntuacion: 9999, jugador: "jugador 1"};
-var jugador2 = { ranking: 2, puntuacion: 8888, jugador: "jugador 1"};
-var jugador3 = { ranking: 3, puntuacion: 7777, jugador: "jugador 1"};
-var jugador4 = { ranking: 4, puntuacion: 6666, jugador: "jugador 1"};
-var jugador5 = { ranking: 5, puntuacion: 5555, jugador: "jugador 1"};
-var jugador6 = { ranking: 6, puntuacion: 4444, jugador: "jugador 1"};
-var jugador7 = { ranking: 7, puntuacion: 3333, jugador: "jugador 1"};
-var jugador8 = { ranking: 8, puntuacion: 2222, jugador: "jugador 1"};
-var jugador9 = { ranking: 9, puntuacion: 1111, jugador: "jugador 1"};
-var jugador10 = { ranking: 10, puntuacion: 0000, jugador: "jugador 1"};
+var jugador2 = { ranking: 2, puntuacion: 8888, jugador: "jugador 2"};
+var jugador3 = { ranking: 3, puntuacion: 7777, jugador: "jugador 3"};
+var jugador4 = { ranking: 4, puntuacion: 6666, jugador: "jugador 4"};
+var jugador5 = { ranking: 5, puntuacion: 5555, jugador: "jugador 5"};
+var jugador6 = { ranking: 6, puntuacion: 4444, jugador: "jugador 6"};
+var jugador7 = { ranking: 7, puntuacion: 3333, jugador: "jugador 7"};
+var jugador8 = { ranking: 8, puntuacion: 2222, jugador: "jugador 8"};
+var jugador9 = { ranking: 9, puntuacion: 1111, jugador: "jugador 9"};
+var jugador10 = { ranking: 10, puntuacion: 0000, jugador: "jugador 10"};
 
 var jugadores = [jugador1,jugador2,jugador3,jugador4,jugador5,jugador6,jugador7,jugador8,jugador9,jugador10];
 
-app.use(bodyParser.json());
+app.use(bodyparser.json());
 
-app.get(apiBaseURL+'/jugadores',function(req,res)
+app.get(apiBaseURL+'/jugadores', function (req,res)
 {
 	console.log("New GET");
 	res.json(jugadores);
 });
 
-app.post(apiBaseURL+'/jugadores',function(req,res){
+app.post(apiBaseURL+'/jugadores', function (req,res){
 	var jugador = req.body;
 
 	console.log("New POST");
-	
-	res.push(jugador);
+	console.log(" Data: " + jugador);
+	jugadores.push(jugador);
 	res.sendStatus(200);
 });
 
-app.delete(apiBaseURL+'/jugadores',function(req,res){
-	var jugador = req.body;
+app.delete(apiBaseURL+'/jugadores', function (req,res){
+	var jugador = req.body.Data;
+	console.log(" new DELETE");
+	console.log(" Data: " + jugador);
 
-	console.log("New DELETE");
-	res.remove(jugador);
-    res.sendStatus(200);
+    for (i = 0; i < jugadores.length; i++) {
+
+        if (jugadores[i].name == req.body.name) {
+
+            jugadores.splice(i);
+
+        }
+
+    } 
+
+    res.sendStatus(200); 
 
 });
 
 
 app.listen(1000);
+
+console.log("Running...");
