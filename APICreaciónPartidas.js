e595f911acf9aa458a828b70ae45f8dae0b829f4
Luis <luizzisgs@gmail.com>
2016-11-15 18:12:28 +0100

    update
    change apiBaseUrl to apiBaseURL

--- APICreaciónPartidas.js
+++ APICreaciónPartidas.js
@@ -4,7 +4,7 @@ var express = require('express');
 var bodyParser = require ('body-parser');
 
 var app = express();
-var apiBaseUrl="/api/v1" //versión de la api.
+var apiBaseURL="/api/v1" //versión de la api.
 
 
 
@@ -13,13 +13,13 @@ var listaPartidas=[partida];
 
 app.use(bodyParser.json());
 
-app.get(apiBaseUrl+'/games',function(req,res){ //request y response 
+app.get(apiBaseURL+'/games',function(req,res){ //request y response 
     console.log("NEW GET");
     res.send(listaPartidas);
     //
 });
 
-app.post(apiBaseUrl+'/games',function(req,res){
+app.post(apiBaseURL+'/games',function(req,res){
     var partidas = req.body;
     console.log("NEW POST");
     console.log("DATA: "+req.body);
@@ -32,14 +32,14 @@ app.post(apiBaseUrl+'/games',function(req,res){
 
 
 
-app.delete(apiBaseUrl+'/games',function(req,res){
+app.delete(apiBaseURL+'/games',function(req,res){
     console.log("Delete");
     listaPartidas.splice(0,listaPartidas.length);
         res.sendStatus(200);
 
 });
 
-app.put(apiBaseUrl+'/games/:player',function(req,res){
+app.put(apiBaseURL+'/games/:player',function(req,res){
     console.log("UPDATE");
     for(i=0;i<listaPartidas.length;i++){
       if(  listaPartidas[i].player==req.params.player){
