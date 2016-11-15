48af0d0ffca7c657511b478f6fede917a8df2044
Micro-N-PC\Nacho <yoiiii121@gmail.com>
2016-11-15 13:01:20 +0100

    change buildpack heroku

--- APIClasificacionGeneral.js
+++ APIClasificacionGeneral.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyparser = require("body-parser");
 var app = express();
 var apiBaseURL = "/api/v1";
+var encontrado = false;
 
 //Inputing datas
 var jugador1 = { ranking: 1, puntuacion: 9999, jugador: "jugador 1"};
@@ -16,14 +17,29 @@ var jugador9 = { ranking: 9, puntuacion: 1111, jugador: "jugador 9"};
 var jugador10 = { ranking: 10, puntuacion: 0000, jugador: "jugador 10"};
 //Inputing values in a list
 var jugadores = [jugador1,jugador2,jugador3,jugador4,jugador5,jugador6,jugador7,jugador8,jugador9,jugador10];
-
+app.set('port', (proccess.env.PORT || 5000));
 app.use(bodyparser.json());
 
 //Geting all players
-app.get(apiBaseURL+'/jugadores', function (req,res)
+/*app.get(apiBaseURL+'/jugadores', function (req,res)
 {
 	console.log("New GET");
 	res.json(jugadores);
+});*/
+
+//Geting the ID of a single player
+app.get(apiBaseURL+'/jugadores/:id', function (req,res)
+{
+	console.log("New ID GET");
+	i = 0;
+	encontrado = false;
+	while (i < jugadores.length && !encontrado) {
+		if(jugadores[i].name == req.params.id){
+			res.json(jugadores[i]);
+			encontrado = true;
+		}
+		i++;
+	}
 });
 
 //Posting all players to be writen
@@ -36,10 +52,25 @@ app.post(apiBaseURL+'/jugadores', function (req,res){
 	res.sendStatus(200);
 });
 
+//Putting the ID of a single player
+app.put(apiBaseURL+'jugadores/:id',function(res,req){
+	console.log("New ID PUT");
+	i = 0;
+	encontrado = false;
+	while(i < jugadores.length && !encontrado){
+		if(jugadores[i].name == req.params.id){
+			friends[i].ranking = req.body.ranking;
+			friends[i].jugador = req.body.jugador;
+			encontrado = true;
+		}
+		i++;
+	}
+
+});
+
 //Deleting all players
 app.delete(apiBaseURL+'/jugadores', function (req,res){
 	console.log(" new DELETE");
-
     for (i = 0; i < jugadores.length; i++) {
 
         if (jugadores[i].name == req.body.name) {
@@ -49,8 +80,23 @@ app.delete(apiBaseURL+'/jugadores', function (req,res){
     res.sendStatus(200); 
 });
 
+//Deleting the ID of a single player
+app.delete(apiBaseURL+'/jugadores/:id',function(req,res){
+	console.log(" new ID DELETE");
+	encontrado = false;
+	i = 0;
+	while (i < jugadores.length && !encontrado) {
+        if (jugadores[i].name == req.params.id) {
+            jugadores.splice(i);
+        }
+        i++;
+    } 
+});
 
-//Listening port 1000
-app.listen(1000);
+//app.listen(1000);
+var port = app.get('port');
+app.listen(port,function()){
+	console.log('Node app is running on port', port);
+}
 
 console.log("Running...");
