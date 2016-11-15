e1e4ad91d1078bd2b3273c1f0e1504426fbe59bb
Luis <luizzisgs@gmail.com>
2016-11-15 17:34:28 +0100

    Update APICreaciónPartidas.js

--- APICreaciónPartidas.js
+++ APICreaciónPartidas.js
@@ -1,53 +1,58 @@
+//API MADE BY LUIS MUÑOZ SAAVEDRA
 var express = require('express');
+
 var bodyParser = require ('body-parser');
 
 var app = express();
-var apiBaseUrl="api/v1" //versión de la api.
-//var palabras = {palabra1 :"",palabra2: "",palabra3 :"",palabra4: "",palabra5 :"",palabra6 :""
-//                ,palabra6 :"",palabra7 :"",palabra8 :"",palabra9 :"",palabra0 :""}
+var apiBaseUrl="/api/v1" //versión de la api.
 
-partida0 = {usuario:"luis",fecha:"11/11/2016"};
-var partida = {usuario:"",fecha:""}
-var partidas=[];
+
+
+partida= {"player" : "luis", "date":"11/11/2016"};
+var listaPartidas=[partida];
 
 app.use(bodyParser.json());
 
-app.get(apiBaseUrl+'/partidas',function(req,res){ //request y response 
+app.get(apiBaseUrl+'/games',function(req,res){ //request y response 
     console.log("NEW GET");
-    res.send(partidas);
-        res.sendStatus(200);
-
+    res.send(listaPartidas);
+    //
 });
 
-app.post(apiBaseUrl+'/partidas',function(req,res){
+app.post(apiBaseUrl+'/games',function(req,res){
     var partidas = req.body;
     console.log("NEW POST");
     console.log("DATA: "+req.body);
-    partidas.push(partida);
+    console.log(JSON.stringify(partidas));
+    listaPartidas.push(partidas);
     res.send(partidas);
-        res.sendStatus(200);
+    //res.sendStatus(200);
 
 });
 
 
 
-app.delete(apiBaseUrl+'/partidas',function(req,res){
+app.delete(apiBaseUrl+'/games',function(req,res){
     console.log("Delete");
-    partidas.slice(0,partidas.length());
+    listaPartidas.splice(0,listaPartidas.length);
         res.sendStatus(200);
 
 });
 
-/*app.put(apiBaseUrl+'/partidas/:partida',function(req,res){
+app.put(apiBaseUrl+'/games/:player',function(req,res){
     console.log("UPDATE");
-    for(i=0;i<partidas.length;i++){
-      if(  partidas[i].usuario==req.params.id){
+    for(i=0;i<listaPartidas.length;i++){
+      if(  listaPartidas[i].player==req.params.player){
+             console.log(JSON.stringify(req.params.player));
+            listaPartidas[i].date=req.body.date;
+            console.log(JSON.stringify(req.body.date));
           }
+    
     }
     res.sendStatus(200);
 
 });
-*/
+
 
 app.listen(65000);
 console.log("Running...")
