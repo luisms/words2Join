ab4f590ed323ce29f951a428925ae71cb282d012
Micro-N-PC\Nacho <yoiiii121@gmail.com>
2017-01-15 13:29:32 +0100

    Arreglar Fallo en NavBar #67

--- public/controllers/home-ctrl.js
+++ public/controllers/home-ctrl.js
@@ -1,13 +1,40 @@
 angular.module("words2JoinAPP")
-.controller("home-ctrl",function($scope,$location){
-    console.log("game controller");
-
-    $scope.playGame = function(){
-            console.log($scope.newGame);
-            $location.path("/individualGames/"+$scope.newGame.player);           
-    }   
-    $scope.profile = function(){
-            console.log($scope.newGame);
-            $location.path("/profile/"+$scope.newGame.player);           
-    }   
-});
+        .controller("home-ctrl", function ($http, $scope, $rootScope, $routeParams, $location, auth, store) {
+                console.log("game controller");
+                var vm = this;
+                vm.logout = $scope.logout;
+                vm.auth = auth;
+                $scope.playGame = function () {
+                        console.log($rootScope.username);
+                        $location.path("/individualGames/" + $rootScope.username);
+                }
+                $scope.profile = function () {
+                        console.log($rootScope.username);
+                        $location.path("/profile/" + $rootScope.username);
+                }
+                $scope.rank = function () {
+                        console.log($rootScope.username);
+                        $location.path("/ranking");
+                }
+                $scope.logout = function () {
+                        if ($rootScope.isAuth0 == false) {
+                                $http.get("/api/v1/logout").then(function (response) {
+                                        console.log("Logout done ");
+                                        console.log("Status: " + response.status);
+                                        $rootScope.isLogged = false;
+                                        $location.path("/");
+                                });
+                        } else {
+                                // The signout method on the auth service
+                                // sets isAuthenticated to false but we
+                                // also need to remove the profile and
+                                // token from local storage
+                                auth.signout();
+                                store.remove('profile');
+                                store.remove('token');
+                                console.log("Logout done ");
+                                $rootScope.isLogged = false;
+                                $location.path('/');
+                        }
+                }
+        });
