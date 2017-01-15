6ea21cec92ea4e3f114c0a564bd90d1c79b9eb09
Micro-N-PC\Nacho <yoiiii121@gmail.com>
2017-01-15 19:45:32 +0100

    Change test port

--- tests/e2e/T02-newGame.js
+++ tests/e2e/T02-newGame.js
@@ -1,17 +1,25 @@
 describe('Add contact', function () {
     it('should show add a new contact', function () {
-        browser.get("http://localhost:5000/#/users/jose", 2000, function (req, res) {
+        browser.get("http://localhost:10000/#/users/jose", 2000, function (req, res) {
 
             element.all(by.repeater("word in words")).then(function (initialWords) {
                 element(by.model("newWord")).sendKeys("click");
+                element(by.css("[value='addWord']")).click();
                 element(by.model("newWord")).sendKeys("raton");
+                element(by.css("[value='addWord']")).click();
                 element(by.model("newWord")).sendKeys("vino");
+                element(by.css("[value='addWord']")).click();
                 element(by.model("newWord")).sendKeys("percha");
+                element(by.css("[value='addWord']")).click();
                 element(by.model("newWord")).sendKeys("rima");
+                element(by.css("[value='addWord']")).click();
                 element(by.model("newWord")).sendKeys("sal");
+                element(by.css("[value='addWord']")).click();
                 element(by.model("newWord")).sendKeys("cruz");
                 element(by.model("newWord")).sendKeys("externocleidomastoideo");
+                element(by.css("[value='addWord']")).click();
                 element(by.model("newWord")).sendKeys("esmeralda");
+                element(by.css("[value='addWord']")).click();
                 element(by.model("newWord")).sendKeys("crack");
                 element(by.css("[value='addWord']")).click();
 
