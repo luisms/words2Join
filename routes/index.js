cfe855cc7a60d7bde60416b7808e4bcf7639eb6c
Micro-N-PC\Nacho <yoiiii121@gmail.com>
2017-01-17 13:40:25 +0100

    port changed

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var env = {
   AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
   AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-  AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:5000/callback'
+  AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:10000/callback'
 };
 
 router.get('/loginAuth0',
